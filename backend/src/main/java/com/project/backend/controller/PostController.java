3daec1935b14e8d9443fcac9f589d3ae5628c853
Donghoon Seo <tjehdgns0925@naver.com>
2024-04-22 15:02:59 +0900

    write 기능추가 +개선 +s3추가+ 추가설정1

--- backend/src/main/java/com/project/backend/controller/PostController.java
+++ backend/src/main/java/com/project/backend/controller/PostController.java
@@ -107,7 +107,7 @@ public class PostController {
             return html;
         }
     }
-    //게시글 작성 끝
+    //게시글 작성 완료
 
 //    @GetMapping("/board")
 //    public String showBoard(@RequestParam(value = "category", required = false, defaultValue = "all") String categoryNo, Model model) {
