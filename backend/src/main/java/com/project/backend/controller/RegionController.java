0a2b31a8be4ef8c6291c649ef97bff71a6a21541
giunchoe <z961223@gmail.com>
2024-05-07 09:36:11 +0900

    backend 간 통신 테스트1

--- backend/src/main/java/com/project/backend/controller/RegionController.java
+++ backend/src/main/java/com/project/backend/controller/RegionController.java
@@ -9,8 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -20,6 +26,9 @@ public class RegionController {
     
     @Autowired
     private RegionService regionService;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/findAll")
     public ResponseEntity<List<RegionDto>> findAllRegions() {
@@ -33,4 +42,25 @@ public class RegionController {
         return new ResponseEntity<>(regions, HttpStatus.OK);
     }
     
+
+    @GetMapping("/requestBanks")
+    public ResponseEntity<String> requestBanks() {
+        // module2의 엔드포인트 URL
+        String userUrl = "http://user-service.default.svc.cluster.local:8080/user/banks";
+
+        // module2에 HTTP GET 요청 보내기
+        ResponseEntity<Map> responseEntity = restTemplate.getForEntity(userUrl, Map.class);
+
+        // 응답에서 은행 목록 가져오기
+        Map<String, Object> banksMap = responseEntity.getBody();
+
+        // 여기서 순회하기
+        for (String key : banksMap.keySet()) {
+            Object value = banksMap.get(key);
+            log.info("Key: " + key + ", Value: " + value);
+        }
+
+
+        return new ResponseEntity<>("Banks processed successfully.", HttpStatus.OK);
+    }
 }
