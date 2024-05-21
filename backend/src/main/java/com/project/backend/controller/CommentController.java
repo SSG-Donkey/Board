0521a9fde170d4b9b984fc4e3c646ac0252ac64d
Donghoon Seo <tjehdgns0925@naver.com>
2024-05-21 16:12:04 +0900

    test

--- backend/src/main/java/com/project/backend/controller/CommentController.java
+++ backend/src/main/java/com/project/backend/controller/CommentController.java
@@ -53,7 +53,7 @@ public class CommentController {
                                             @RequestParam("userNo")String userNo,
                                             @RequestParam("commentNo")String commentNo){
 
-        System.out.printf("comment_no = %s , post_no =%s user_no =%s",commentNo,postNo,userNo);
+        System.out.printf("comment_no = %s , post_no =%s user_no =%s\n",commentNo,postNo,userNo);
         int comment_no=Integer.parseInt(commentNo);
         CommentDto commentDto = new CommentDto();
         commentDto.setPostNo(postNo);
@@ -62,8 +62,9 @@ public class CommentController {
         int res2 =commentService.validateComment(commentDto);
 
         Map<String,String> response =new HashMap<>();
-        System.out.printf(" res1=%d ,res2=%d\n",commentNo,res2);
+        System.out.printf(" res2 결과 :%d\n",res2);
         if(comment_no== res2){
+            int res1 =commentService(commentDto);
             response.put("message", "댓글 삭제 완료하였습니다.");
             response.put("redirectUrl", "/boardDetail.html?postNo=" + postNo);
 
