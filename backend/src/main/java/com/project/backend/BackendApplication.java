9e2bf1844263c582df48171146926f7bb464e3ef
giunchoe <z961223@gmail.com>
2024-05-27 16:22:20 +0900

    redis 삭제

--- backend/src/main/java/com/project/backend/BackendApplication.java
+++ backend/src/main/java/com/project/backend/BackendApplication.java
@@ -2,10 +2,8 @@ package com.project.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession
 public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
